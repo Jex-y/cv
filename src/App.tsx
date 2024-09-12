007991b6f14838cf7333bb7d75b1b84b4921a857
Ed Jex <edwardjex@live.co.uk>
2024-09-12 23:10:45 +0100

    fix workflow again

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
 import { Mail, Phone } from "lucide-react";
 
+function Github() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        fill="currentColor"
+        stroke-width="0"
+      >
+        <title>GitHub</title>
+        <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+      </svg>
+    </>
+  );
+}
+
 function IconLink({
   icon,
   href,
@@ -70,20 +89,20 @@ function Skill({ name, level, subskills, description }: SkillProps) {
 
         <div className="h-2 w-32 bg-gray-400 rounded-lg">
           <div
-            className="h-full bg-blue-500 rounded-lg"
+            className="h-full bg-sky-500 rounded-lg"
             style={{ width: `${level}%` }}
           />
         </div>
       </div>
       <div className="ml-4">
         {subskills && (
-          <ul className="ml-4 text-gray-600 list-disc text-sm">
+          <ul className="ml-4 list-disc text-sm">
             {subskills.map((subskill) => (
               <li key={subskill}>{subskill}</li>
             ))}
           </ul>
         )}
-        {description && <p>{description}</p>}
+        {description && <p className="text-sm">{description}</p>}
       </div>
     </div>
   );
@@ -103,6 +122,11 @@ function CV() {
           <li>
             <IconLink icon={<Phone />} href="tel:+447917 572525">
               <span>+44 7917 57 2525</span>
+            </IconLink>
+          </li>
+          <li>
+            <IconLink icon={<Github />} href="https://github.com/Jex-y">
+              <span>GitHub/Jex-y</span>
             </IconLink>
           </li>
         </ul>
@@ -136,7 +160,7 @@ function CV() {
               description="I have experience building web applications with modern technologies. This CV is built with React and TailwindCSS."
             />
             <Skill
-              name="Devops"
+              name="DevOps"
               level={80}
               subskills={[
                 "Continous Integration and Deployment",
@@ -172,17 +196,6 @@ function CV() {
         <div className="grid gap-4">
           <Section title="Experience">
             <Experience
-              title="Cisco Web Development Intern"
-              subtitle="Webex Web Client"
-              date="Summer 2022"
-            >
-              <p className="text-sm mx-2">
-                I worked on the Webex web client team, developing new features
-                and fixing bugs. Through this I gained experience with React and
-                Redux and learned how to work on software as part of a team.
-              </p>
-            </Experience>
-            <Experience
               title="Undergraduate Teaching Assistant"
               subtitle="Department of Computer Science"
               date="2023 - 2024"
@@ -195,6 +208,17 @@ function CV() {
                 Students have a wide range of abilities and backgrounds,
                 requiring me to be able to explain concepts in multiple ways and
                 adapt to different learning styles.
+              </p>
+            </Experience>
+            <Experience
+              title="Cisco Web Development Intern"
+              subtitle="Webex Web Client"
+              date="Summer 2022"
+            >
+              <p className="text-sm mx-2">
+                I worked on the Webex web client team, developing new features
+                and fixing bugs. Through this I gained experience with React and
+                Redux and learned how to work on software as part of a team.
               </p>
             </Experience>
 
